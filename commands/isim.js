5d70a8996bf58e20cd8fac3c4cfa383c603adf00
Glitch (clerance-v12-altyapi) <none>
2020-07-19 12:51:29 +0000

    👨‍👨‍👦💋 Checkpoint ./commands/erkek.js:8975294/1317 ./commands/kız.js:8975294/1221 ./komutlar/isim.js:8975294/1071 ./commands/isim.js:8975294/240 ./ayarlar.json:8975294/118

---
+++ commands/isim.js
@@ -0,0 +1,28 @@
+const Discord = require('discord.js');
+
+exports.run = async (client, message, args) => {
+
+ if (!message.member.roles.cache.has("rol id ")&& !message.member.hasPermission("ADMINISTRATOR")) return message.channel.send(`Bu komutu kullanabilmek için \`Rolün İsmi\` yetkisine sahip olmasınız.`);
+  let member = message.mentions.members.first()
+  let isim = args.slice(1).join(" ")
+  if (!member) return message.channel.send('Bir üye etiketlemelisin kanka')
+  if (!isim) return message.channel.send('Bir isim yazmalısın kanka')
+  member.setNickname(`• ${isim}`)
+  const embed = new Discord.MessageEmbed()
+  .addField(`• Kullanıcının takma adı değiştirildi.`, `Kullanıcı adını başarıyla \`• ${isim}\` olarak ayarladım!`)
+  .setFooter(`Komutu kullanan yetkili : ${message.author.username}`)  
+  .setThumbnail(client.user.avatarURL)
+  message.channel.send(embed)
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: true,
+  aliases: ['isim'],
+  permLevel: 0
+}
+exports.help = {
+  name: 'nick',
+  description: "Birinin nickini değiştirir.",
+  usage: 'nick <yeni nick>'
+}
---
+++ commands/kız.js
@@ -0,0 +1,34 @@
+const Discord = require('discord.js');
+const db = require("quick.db")
+
+exports.run = async (client, message, args) => {
+
+if (!message.member.roles.cache.has("rol id")&& !message.member.hasPermission("ADMINISTRATOR")) return message.channel.send(`Bu komutu kullanabilmek için \`kullanacağı rol isimi\` yetkisine sahip olmasınız.`);
+  let kullanıcı = message.mentions.users.first()
+  if (!kullanıcı) return message.channel.send('Kullanıcıyı etiketlemeyi unuttun kanka.')
+  let rol = message.mentions.roles.first()
+  let member = message.guild.member(kullanıcı)
+ member.roles.add('');//verilecek rol
+   member.roles.remove('');//alınacak rol
+   member.roles.remove('');//alınacak rol
+  let embed = new Discord.MessageEmbed()
+  .setColor('BLACK')
+  .setDescription(`${kullanıcı} **üyesine** <@&kız rol id> **rolü verildi!**`)
+  .setFooter(`Komutu kullanan yetkili : ${message.author.username}`) 
+  message.react('emoji id') // verilecek tepki
+  return message.channel.send(embed)
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: true,
+  aliases: ["kadın","k"],
+  kategori: "KULLANICI KOMUTLARI",
+  permLevel: 0
+}
+
+exports.help = {
+  name: 'kız',
+  description: "Hadi erkek olalımm",
+  usage: 'erkek'
+}
